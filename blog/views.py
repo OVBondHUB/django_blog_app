dd8408cf363dcec8bc829a38092c7ccc879e23f6
bondarenko <bondarenko@rixo.cz>
2020-07-08 20:35:17 +0200

    master: Added Tag model and template & many to many relation with post model

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 
 
 def post_list(request):
@@ -12,3 +12,12 @@ def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blog/post_detail.html', context={'post': post})
 
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags.html', context={'tags': tags})
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
