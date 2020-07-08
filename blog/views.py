d39787d1944db7ce5275a69d4114b0907cecdc0b
bondarenko <bondarenko@rixo.cz>
2020-07-08 17:26:23 +0200

    master: Added Post detail template

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts, "request": request})
 
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
+
