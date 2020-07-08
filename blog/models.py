20289abe0bd275f259f7044cc335f649d6f63c4a
bondarenko <bondarenko@rixo.cz>
2020-07-08 17:35:07 +0200

    master: Added post detail url path && simplified url params handling

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
@@ -6,6 +7,10 @@ class Post(models.Model):
     slug = models.SlugField(max_length=150, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
+
+    # Convention method, should be implement is necessary obj ref in templates
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list_url'),
-    path('post/<str:slug>',post_detail, name='post_detail_url')
+    path('post/<str:slug>', post_detail, name='post_detail_url')
 ]
